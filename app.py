ac3fcef1321b21a9a36bd2dd10c0d8cd1094d553
Sandra Rodgers <sandra.rodgers@deepgram.com>
2023-11-13 15:24:58 -0600

    theme colors etc

--- app.py
+++ app.py
@@ -3,6 +3,25 @@ from streamlit_player import st_player
 from helpers import get_caption_at_time, parse_srt, parse_webvtt
 from captions import transcribe_file
 
+custom_header = """
+    <style>
+        .custom-header {
+            display: flex;
+            flex-direction: column;
+        }
+        .divider {
+            flex-grow: 1;
+            height: 1px;
+            background: linear-gradient(to right, #FF2EEA, #4B3CFF);
+            margin-bottom: 1rem;
+        }
+    </style>
+    <div class="custom-header">
+        <h1>SRT VTT Captions</h1>
+        <div class="divider"></div>
+    </div>
+"""
+
 
 # STATE
 if "selections" not in st.session_state:
@@ -45,17 +64,19 @@ def get_captions(url):
 
 def run():
     st.set_page_config(
-        page_title="Deepgram Captions",
-        page_icon="👋",
+        page_title="SRT VTT Captions",
+        page_icon="🗣️",
     )
 
-    st.header("Deepgram Captions", divider="rainbow")
-
-    # st.write(st.session_state)
+    st.markdown(custom_header, unsafe_allow_html=True)
+    st.markdown(
+        """This demo uses the open source python package [deepgram-captions](https://github.com/deepgram/deepgram-python-captions) to create webvtt and srt captions from the Deepgram speech-to-text API transcription response. The package can also be used with other speech-to-text transcription formats such as whisper-timestamped. (See the package readme for more info.)""",
+        unsafe_allow_html=False,
+    )
 
     caption_choice = st.selectbox("Select your type of captions:", ("srt", "webvtt"))
 
-    url = st.text_input("First URL", "https://www.youtube.com/watch?v=kibx5BR6trA")
+    url = st.text_input("First URL", "https://www.youtube.com/watch?v=MPmx09S4cLw")
 
     video_options = st.session_state.video_options
 
@@ -77,12 +98,12 @@ def run():
             srt_captions_dict = parse_srt(st.session_state.captions["srt"])
             current_caption_srt = get_caption_at_time(srt_captions_dict, played_seconds)
             st.markdown(
-                f'<span style="display: block; height: 100px; text-align: center; font-size: 1.3em; color:purple; font-weight:600;">{current_caption_srt}</span>',
+                f'<span style="display: block; height: 100px; text-align: center; font-size: 1.7em; color:#FF2EEA; font-weight:600;">{current_caption_srt}</span>',
                 unsafe_allow_html=True,
             )
             if st.session_state.youtube_player["data"]["played"] > 0:
-                st.text("Full srt captions for the entire video:")
-                st.text(st.session_state.captions["srt"])
+                st.subheader("Full srt captions for the entire video:")
+                st.code(st.session_state.captions["srt"], language="text")
         #   Display and use webvtt captions in the demo:
         if caption_choice == "webvtt":
             webvtt_captions_dict = parse_webvtt(st.session_state.captions["webvtt"])
@@ -91,12 +112,12 @@ def run():
                 webvtt_captions_dict, played_seconds
             )
             st.markdown(
-                f'<span style="display: block; height: 100px; text-align: center; font-size: 1.3em; color:blue; font-weight:600;">{current_caption_webvtt}</span>',
+                f'<span style="display: block; height: 100px; text-align: center; font-size: 1.7em; color:#13EF93; font-weight:600;">{current_caption_webvtt}</span>',
                 unsafe_allow_html=True,
             )
             if st.session_state.youtube_player["data"]["played"] > 0:
-                st.text("Full webvtt captions for the entire video:")
-                st.text(st.session_state.captions["webvtt"])
+                st.subheader("Full webvtt captions for the entire video:")
+                st.code(st.session_state.captions["webvtt"], language="text")
 
 
 if __name__ == "__main__":
